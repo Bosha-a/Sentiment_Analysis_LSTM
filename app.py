e6c3d4842039ce920c5c447d78ee213d8faaa025
Abdallah Beshary <abdallahuuuio221@gmail.com>
2025-01-25 14:09:30 +0200

    update

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st 
+import pickle as pkl 
+from helper import normalize_text
+import tensorflow as tf 
+from tensorflow.keras.models import load_model
+
+model = load_model('C:\Users\besha\OneDrive\Desktop\ODC\Sentiment_Lstm\Sentiment_Analysis_LSTM\model_with_embidding.keras')
+
+title = st.title('Welcome to Your Sentiment Analysis Application')
+
+text = st.text_input('Enter your text here:')
+text = normalize_text(text)
+
+
+button = st.button('Predict')
+
+dict = {1 : 'Positive', 0 : 'Negative'}
+
+if button:
+    prediction = model.predict(text)[0]
+    sentiment = dict[prediction]
+    st.write(sentiment)
+    
